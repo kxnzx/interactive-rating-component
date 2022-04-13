b13ba09315416975bba4c32c91d9878e0ceef33a
kxnzx <k.bousboa@gmail.com>
2022-04-13 15:44:48 +0200

    edit

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-"use strict";
-// This tells the browser to pay attention to the following elements:
-const cardRatingForm = document.querySelector(".card_rating-form"); // Rating Card //
-const radioButton = document.querySelectorAll(".radio_button"); // Rating Button //
-const cardButton = document.querySelector(".card_button"); // Submit Button //
-const cardPopUp = document.querySelector(".card_pop-up"); // Thank You Card //
-const cardScore = document.querySelector(".card_score"); // Card Score //
-//let rating = 0;
-//console.log();
+const ratingValue = document.getElementById(rating_value);
+const submitButton = document.getElementById(submit_button);
+const outputValue = document.getElementById(output_value);
+console.log(outputValue);
 
-// This tells the browser to listen out for clicks on the Rating Buttons & Submit Button:
-cardButton.addEventListener("click", submit);
-radioButton.forEach((btn) => {
-  btn.addEventListener("click", rate);
-});
-
-// function doSomething(){}
-// This is what the browser should do when the click event occurs:
-function submit() {
-  cardRatingForm.classList.add("obscure"); // Add display: none; on the Rating Card
-  cardPopUp.classList.remove("obscure"); // Remove display: none; on the Thank You Card
+function result() {
+  outputValue.innerHTML = ratingValue.value;
 }
-// Make the radio button grey when clicked (all radio buttons become grey => need to fix this: only the clicked radio button has to become grey)
-function rate(event) {
-  radioButton.forEach((btn) => {
-    btn.classList.add("clicked_radio_button");
-  });
 
-  // How to make the browser remember the value of the clicked radio buttons and loop it back?
-}
+submitButton.addEventListener("click", result);
