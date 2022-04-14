c3fa552c1c5561a7028ac88bea8d5894009d6b38
kxnzx <k.bousboa@gmail.com>
2022-04-15 00:19:45 +0200

    edit

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // This tells the browser to pay attention to the following elements:
 const cardForm = document.querySelector(".card_form");
-const form = document.querySelector("form");
 const cardFormMessage = document.querySelector(".card_form_message");
 const button = document.querySelector("button");
-const radioButton = document.querySelector(".radio_button");
 
 // function doSomething(){}
 // This is what the browser should do when the onclick event occurs:
@@ -25,9 +23,3 @@ function submit() {
   cardForm.classList.add("hide"); // Add display: none; on specified element
   cardFormMessage.classList.remove("hide"); // Remove display: none; on specified element
 }
-
-// Make the radio button grey when clicked (all radio buttons become grey => need to fix this: only the clicked radio button has to become grey)
-/*function rate(event) {
-  radioButton.forEach((btn) => {
-    btn.classList.add("clicked");
-  });*/
